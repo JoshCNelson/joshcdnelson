ca137f8883f2c8ad3160407bb6e477a52d32de00
Josh Nelson <joshcdnelson@gmail.com>
2020-04-05 20:32:12 -0400

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Josh Nelson</h1>
+      <ul>
+        <li>Projects</li>
+        <li>Blog</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
