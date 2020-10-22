b9b564732ffd5d6cb4a3ab61db6d353d45621ebb
Josh Nelson <joshcdnelson@gmail.com>
2020-10-21 20:25:23 -0400

    done with first pass mvp

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { FaGithub } from 'react-icons/fa'
-import './home.css'
+import React from "react";
+import "./home.css";
 
 const Home = () => {
   return (
@@ -12,50 +11,74 @@ const Home = () => {
         <div className="greeting">
           <p>Hello there!</p>
           <p>My name is Josh Nelson</p>
-          <p>I am a Full Stack Engineer and co-creator of <a href="http://plinkojs.com/about">PlinkoJS</a>, a real-time, physics-based browser multiplayer</p>
+          <p>
+            I am a Full Stack Engineer and co-creator of{" "}
+            <a href="http://plinkojs.com/about">PlinkoJS</a>, a real-time,
+            physics-based browser multiplayer
+          </p>
         </div>
       </div>
       <div className="plinko">
         <div className="description">
           <p>PlinkoJS</p>
+          <p>PlinkoJS is a real-time, multiplayer browser game.</p>
           <p>
-            PlinkoJS is a real-time, physics-based, multiplayer browser game.
+            Read the case study to learn about the challenges of building a
+            real-time, interactive application
           </p>
           <p>
-            PlinkoJS is a second paragrtaph on it and goes over the other stuff. Should I link the slides.
+            <a className="case-study" href="www.plinkojs.com/about">
+              Read Case Study
+            </a>
           </p>
-          <p><a className="case-study" href="www.plinkojs.com/about">Read Case Study</a></p>
         </div>
         <div className="gameplay-wrapper">
           <figure>
-            <img className="gameplay" src="https://media.giphy.com/media/1rRkqtfEgsK57mgN52/giphy.gif" alt="Plinko Gameplay" />
+            <img
+              className="gameplay"
+              src="https://media.giphy.com/media/1rRkqtfEgsK57mgN52/giphy.gif"
+              alt="Plinko Gameplay"
+            />
           </figure>
         </div>
       </div>
       <div className="miscellaneous">
         <div className="other-projects">
+          <h3>Other Projects</h3>
           <ul>
             <li>
-              <p><span>Micro-Tix</span>: a microservices app for buying tickets <a href="#"><FaGithub /></a></p>
+              <p>
+                <a href="https://github.com/JoshCNelson/ticketing-microservices">
+                  MicroTix
+                </a>
+                : A microservices app for buying tickets
+              </p>
+              <p className="tech">
+                <span>Tech</span>: Docker, Kubernetes, NATS, Typescript, Node,
+                NextJS, Redis, MongoDB
+              </p>
             </li>
             <li>
-              <p><span>Reaction</span>: a trello-clone built in Rails and React <a href="#"><FaGithub /></a></p>
-            </li>
-            <li>
-              <p><span>Reaction</span>: a trello-clone built in Rails and React <a href="#"><FaGithub /></a></p>
+              <p>
+                <a href="https://github.com/ryannmcq/reaction">Reaction</a>: A
+                Trello-clone
+              </p>
+              <p className="tech">
+                <span>Tech</span>: React, Redux, Rails, PostgreSQL
+              </p>
             </li>
           </ul>
         </div>
         <div className="technology">
-          <ul>
+          {/* <ul>
             <li>some tech</li>
             <li>some tech</li>
             <li>some tech</li>
-          </ul>
+          </ul> */}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
